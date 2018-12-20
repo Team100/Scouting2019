e9f1622bd737c7c326500e08f8e37a239d73e877
Alex Beaver <31376007+alexanderbeaver799710@users.noreply.github.com>
2018-12-20 06:43:01 -0800

    Add Plotly to profile page

--- admin/team/profile.js
+++ admin/team/profile.js
@@ -18,4 +18,26 @@ function populatePage(){
 	for(var i = 0; i < teamNumberInstances.length; i++){
 		teamNumberInstances[i].innerText = pageParams.team;
 	}
+
+	var smartScoreInstances = document.getElementsByClassName("data-team-smartscore");
+	for(var i = 0; i < smartScoreInstances.length; i++){
+		smartScoreInstances[i].innerText = "123";
+	}
+	renderTeamScoringData();
+	
+}
+
+function renderTeamScoringData(){
+	// TODO Update to have actual team scoring data
+	var data = [{
+		values: [19, 26, 55],
+		labels: ['Residential', 'Non-Residential', 'Utility'],
+		type: 'pie'
+	  }];
+	  
+	  var layout = {
+		
+	  };
+	  
+	  Plotly.newPlot('scoring', data, layout, {responsive: true});
 }
