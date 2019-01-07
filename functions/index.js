1fb7be4b4e85adea843c81a9645517a2fdcf2a21
Alex Krantz <alex@alexkrantz.com>
2019-01-07 11:34:05 -0800

    Remove remanants of axios dependency

--- functions/index.js
+++ functions/index.js
@@ -1,16 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
-const axios = require("axios");
 
 admin.initializeApp(functions.config().firebase);
 admin.firestore().settings({timestampsInSnapshots: true});
 
 // TODO: get config from database
 const EVENT = "davis";
-const EVENT_KEY = "2018cada";
-
-axios.defaults.headers.common["X-TBA-Auth-Key"] = "UegGTUgQM50rZaDdu5fHXvTMmB9tAjlAQi4spx4u1KKcDLwdjAaQpbgTZmiqnF4y";
-axios.defaults.headers.common["User-Agent"] = "Team 100 FRC Scouting";
 
 // TODO: update for 2019 game
 exports.ingress = functions.https.onRequest((request, response) => {
