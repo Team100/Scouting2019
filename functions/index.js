49b914e36fb42c444f7a602b1ae9b5793399c06e
Alex Krantz <alex@alexkrantz.com>
2019-01-07 10:32:39 -0800

    Add processing to ingress

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,9 @@ const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 admin.firestore().settings({timestampsInSnapshots: true});
 
+const EVENT = "davis";
+
+// TODO: update for 2019 game
 exports.ingress = functions.https.onRequest((request, response) => {
     try {
         // Validate that data exists
@@ -15,17 +18,85 @@ exports.ingress = functions.https.onRequest((request, response) => {
         // Convert query data into JSON
         let data = JSON.parse(request.query.data);
 
-        // Update current value in unprocessed
-        admin.firestore().collection("unprocessed").doc(data.metadata.teamName).update({
-            match: admin.firestore.FieldValue.arrayUnion(data)
-        }).then(() => {
-            // Return success
-            response.status(200).send(JSON.stringify({"status": "success"}));
+        // Validate json data
+        // Currently only for match data
+        // TODO: pit data
+        if (!data.hasOwnProperty("switch")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
             return;
+        } else if (!data.hasOwnProperty("scale")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.hasOwnProperty("exchange")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.hasOwnProperty("disable")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.hasOwnProperty("brownout")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.hasOwnProperty("break")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.hasOwnProperty("crossline")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.hasOwnProperty("endstate")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.hasOwnProperty("metadata")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.metadata.hasOwnProperty("matchNumber")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.metadata.hasOwnProperty("scouter")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        } else if (!data.metadata.hasOwnProperty("teamName")) {
+            response.status(400).send(JSON.stringify({"status": "error", "reason": "Invalid JSON format"}));
+            return;
+        }
+
+        // Update current value in event
+        admin.firestore().collection("events").doc(EVENT).get().then(doc => {
+            if (!doc.exists) {
+                response.status(400).send(JSON.stringify({"status": "error", "reason": "Could not retrieve current events from database"}));
+                return;
+            }
+
+            let team = doc.data()[data.metadata.teamName];
+
+            team.matches.push(data);
+            
+            let sumSwitch = 0;
+            let sumScale = 0;
+            let sumExchange = 0;
+            for (let match of team.matches) {
+                sumSwitch += match.switch.length;
+                sumScale += match.scale.length;
+                sumExchange += match.exchange.length;
+            }
+            team.scoring.switch = sumSwitch/team.matches.length;
+            team.scoring.scale = sumScale/team.matches.length;
+            team.scoring.exchange = sumExchange/team.matches.length;
+
+            // TODO: add processing for lyingIndex, smartScore, & tba-rank
+
+            admin.firestore().collection("events").doc(EVENT).update({"254": team}).then(() => {
+                response.status(200).send(JSON.stringify({"status": "success"}))
+                return;
+            }).catch(err => {
+                // Return generic error and log actual
+                console.error("Error getting current event document:", err);
+                response.status(500).send(JSON.stringify({"status": "error", "reason": "Could not retrieve data from the database"}));
+                return;
+            });
         }).catch(err => {
             // Return generic error and log actual
-            console.error("Error updating document:", err);
-            response.status(500).send(JSON.stringify({"status": "error", "reason": "Could not update the database"}));
+            console.error("Error getting current event document:", err);
+            response.status(500).send(JSON.stringify({"status": "error", "reason": "Could not retrieve data from the database"}));
             return;
         });
     } catch (e) {
