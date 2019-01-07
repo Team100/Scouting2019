e11c525f2a84a97ec67c06c31822c06f297a3c29
Alex Krantz <alex@alexkrantz.com>
2019-01-06 22:06:29 -0800

    Fix automatic returning error

--- functions/index.js
+++ functions/index.js
@@ -4,29 +4,32 @@ const admin = require("firebase-admin");
 admin.initializeApp(functions.config().firebase);
 
 exports.ingress = functions.https.onRequest((request, response) => {
-    // Validate that data exists
-    if (!request.query.hasOwnProperty("data")) {
-        response.status(400).send(JSON.stringify({"status": "error","reason": "Invalid request"}));
-        return;
+    try {
+        // Validate that data exists
+        if (!request.query.hasOwnProperty("data")) {
+            response.status(400).send(JSON.stringify({"status": "error","reason": "Invalid request"}));
+            return;
+        }
+
+        // Convert query data into JSON
+        let data = JSON.parse(request.query.data);
+
+        // Update current value in unprocessed
+        admin.firestore().collection("unprocessed").doc(data.metadata.teamName).update({
+            match: admin.firestore.FieldValue.arrayUnion(data)
+        }).then(() => {
+            // Return success
+            response.status(200).send(JSON.stringify({"status": "success"}));
+            return;
+        }).catch(err => {
+            // Return generic error and log actual
+            console.error("Error updating document:", err);
+            response.status(500).send(JSON.stringify({"status": "error", "reason": "Could not update the database"}));
+            return;
+        });
+    } catch (e) {
+        // Catch all for errors
+        console.error(e);
+        response.status(500).send(JSON.stringify({"status": "error", "reason": "Something went wrong and we're looking into it"}));
     }
-
-    // Convert query data into JSON
-    let data = JSON.parse(request.query.data);
-
-    // Update current value in unprocessed
-    admin.firestore().collection("unprocessed").doc(data.metadata.teamName).update({
-        match: admin.firestore.FieldValue.arrayUnion(data)
-    }).then(() => {
-        // Return success
-        response.status(200).send(JSON.stringify({"status": "success"}));
-        return;
-    }).catch(err => {
-        // Return generic error and log actual
-        console.log("Error updating document:", err);
-        response.status(500).send(JSON.stringify({"status": "error", "reason": "Could not update the database"}));
-        return;
-    });
-
-    // Catch all for errors
-    response.status(500).send(JSON.stringify({"status": "error", "reason": "Something went wrong and we're looking into it"}))
 });
